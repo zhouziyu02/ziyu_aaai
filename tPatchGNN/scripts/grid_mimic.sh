fb2053341c64f0b857d82a201a1074b258a22817
ziy <ziyuzhou30@gmail.com>
2025-06-04 14:50:31 +0800

    restore

--- tPatchGNN/scripts/grid_mimic.sh
+++ tPatchGNN/scripts/grid_mimic.sh
@@ -8,44 +8,40 @@ mkdir -p gridsearch
 
 # Model: simple, Best Epoch:229, Seed: 1, lr: 0.001, batch_size: 32, nhead: 1, tedim: 10, nlayer: 1, K: 2, hid_dim: 16, preconvdim:16
 
-export CUDA_VISIBLE_DEVICES=2 # Assigns physical GPU 0 to be logical GPU 0 for this subshell
-  for seed_val in $(seq 103 110 ); do
-    python run_models.py \
-      --model $model \
-      --align variate \
-      --dataset mimic --state 'def' --history 24 \
-      --patience 50 --batch_size 32 --lr 0.001 \
-      --nhead 1 --tf_layer 1 --nlayer 1 --K 2 \
-      --hid_dim 16 --preconvdim 16 --te_dim 10 \
-      --outlayer Linear --seed $seed_val --gpu 0
-  done
+# export CUDA_VISIBLE_DEVICES=3 # Assigns physical GPU 0 to be logical GPU 0 for this subshell
+#   for seed_val in $(seq 150 170); do
+#     python run_models.py \
+#       --model $model \
+#       --align variate \
+#       --dataset mimic --state 'def' --history 24 \
+#       --patience 50 --batch_size 32 --lr 0.001 \
+#       --nhead 1 --tf_layer 1 --nlayer 1 --K 2 \
+#       --hid_dim 16 --preconvdim 16 --te_dim 10 \
+#       --outlayer Linear --seed $seed_val --gpu 0
+#   done
 
 
-# for batch_size in 32; do
-# for k in 2; do
-# for te_dim in 20; do
-# for nlayer in 1 2; do
-# for preconvdim in 16 32; do
-# for hid_dim in 16 32; do
+for batch_size in 32; do
+for k in 4; do
+for te_dim in 10 20; do
+for nlayer in 1 2; do
+for preconvdim in 16 32; do
 
-# export CUDA_VISIBLE_DEVICES=1
-# python run_models.py \
-#     --model $model \
-#     --align variate \
-#     --dataset mimic --state 'def' --history 24 \
-#     --patience 50 --batch_size $batch_size --lr 1e-3 \
-#     --nhead 1 --tf_layer 1 --nlayer $nlayer --K $k \
-#     --te_dim $te_dim --node_dim 10 --hid_dim $hid_dim --preconvdim $preconvdim \
-#     --outlayer Linear --seed 1 --gpu 0  >> gridsearch/mimic.txt 2>&1
-
-
-# wait
-# done
-# done
-# done
-# done
-# done
-# done
+export CUDA_VISIBLE_DEVICES=3
+python run_models.py \
+    --model $model \
+    --align variate \
+    --dataset mimic --state 'def' --history 24 \
+    --patience 50 --batch_size $batch_size --lr 1e-3 \
+    --nhead 1 --tf_layer 1 --nlayer $nlayer --K $k \
+    --te_dim $te_dim --node_dim 10 --hid_dim 64 --preconvdim $preconvdim \
+    --outlayer Linear --seed 1 --gpu 0   
+ 
+done
+done
+done
+done
+done
 
 
 
